af96f89e35081d0d8e942b8320040f0e06ecb2e0
anchor10 <1129713332@qq.com>
2018-10-16 19:07:46 +0800

    change ad

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <a href="http://www.qust.edu.cn/info/1641/8640.htm" class="qust-ad"><img src="http://www.qust.edu.cn/__local/5/12/C1/62DA6F5ABF108CA4B4BFCBA052B_DCB36174_74157.jpg"></a>
+    <a href="http://app.3733.com/" class="qust-ad"><img src="../static/images/5a.jpg"></a>
     <router-view/>
   </div>
 </template>
@@ -28,10 +28,10 @@ export default {
 .qust-ad{
   display: block;
   width: 100%;
-  height: 80px;
   overflow: hidden;
 }
 .qust-ad img{
   width: 100%;
+  display: block;
 }
 </style>
