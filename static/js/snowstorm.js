ed1b5138e091ed4c1ede8b447441890a96bdff54
anchor10 <1129713332@qq.com>
2018-12-13 13:55:42 +0800

    修改了 snowstorm.js

--- static/js/snowstorm.js
+++ static/js/snowstorm.js
@@ -17,8 +17,10 @@ var snowStorm = (function(window, document) {
   
     this.autoStart = true;          // Whether the snow should start automatically or not.
     this.excludeMobile = true;      // Snow is likely to be bad news for mobile phones' CPUs (and batteries.) Enable at your own risk.
-    this.flakesMax = 128;           // Limit total amount of snow made (falling + sticking)
-    this.flakesMaxActive = 64;      // Limit amount of snow falling at once (less = lower CPU use)
+    // this.flakesMax = 128;           // Limit total amount of snow made (falling + sticking)
+    // this.flakesMaxActive = 64;      // Limit amount of snow falling at once (less = lower CPU use)
+    this.flakesMax = 40;
+    this.flakesMaxActive = 20;
     this.animationInterval = 33;    // Theoretical "miliseconds per frame" measurement. 20 = fast + smooth, but high CPU use. 50 = more conservative, but slower
     this.useGPU = true;             // Enable transform-based hardware acceleration, reduce CPU load.
     this.className = null;          // CSS class name for further customization on snow elements
@@ -31,7 +33,7 @@ var snowStorm = (function(window, document) {
     this.targetElement = null;      // element which snow will be appended to (null = document.body) - can be an element ID eg. 'myDiv', or a DOM node reference
     this.useMeltEffect = true;      // When recycling fallen snow (or rarely, when falling), have it "melt" and fade out if browser supports it
     this.useTwinkleEffect = false;  // Allow snow to randomly "flicker" in and out of view while falling
-    this.usePositionFixed = false;  // true = snow does not shift vertically when scrolling. May increase CPU load, disabled by default - if enabled, used only where supported
+    this.usePositionFixed = true;  // true = snow does not shift vertically when scrolling. May increase CPU load, disabled by default - if enabled, used only where supported
     this.usePixelPosition = false;  // Whether to use pixel values for snow top/left vs. percentages. Auto-enabled if body is position:relative or targetElement is specified.
   
     // --- less-used bits ---
@@ -43,7 +45,7 @@ var snowStorm = (function(window, document) {
     this.flakeHeight = 8;           // Max pixel height reserved for snow element
     this.vMaxX = 5;                 // Maximum X velocity range for snow
     this.vMaxY = 4;                 // Maximum Y velocity range for snow
-    this.zIndex = 0;                // CSS stacking order applied to each snowflake
+    this.zIndex = 10000;                // CSS stacking order applied to each snowflake
   
     // --- "No user-serviceable parts inside" past this point, yadda yadda ---
   
@@ -59,7 +61,7 @@ var snowStorm = (function(window, document) {
     windOffset = 1,
     windMultiplier = 2,
     flakeTypes = 6,
-    fixedForEverything = false,
+    fixedForEverything = true,
     targetElementIsRelative = false,
     opacitySupported = (function(){
       try {
@@ -624,7 +626,7 @@ var snowStorm = (function(window, document) {
         storm.usePixelPosition = true;
       }
       storm.resizeHandler(); // get bounding box elements
-      storm.usePositionFixed = (storm.usePositionFixed && !noFixed && !storm.flakeBottom); // whether or not position:fixed is to be used
+      storm.usePositionFixed = (storm.usePositionFixed); // whether or not position:fixed is to be used
       if (window.getComputedStyle) {
         // attempt to determine if body or user-specified snow parent element is relatlively-positioned.
         try {
@@ -649,10 +651,14 @@ var snowStorm = (function(window, document) {
       storm.events.remove(isIE?document:window,'mousemove',doDelayedStart);
     }
   
+    // function doStart() {
+    //   if (!storm.excludeMobile || !isMobile) {
+    //     doDelayedStart();
+    //   }
     function doStart() {
-      if (!storm.excludeMobile || !isMobile) {
+      // if (!storm.excludeMobile) {
         doDelayedStart();
-      }
+      // }
       // event cleanup
       storm.events.remove(window, 'load', doStart);
     }
