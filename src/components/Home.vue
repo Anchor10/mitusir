3b74c6f2d178d8716bd56c22e6728b3966b462a1
anchor10 <1129713332@qq.com>
2018-12-21 16:18:08 +0800

    add 照片墙

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -28,7 +28,7 @@
                         </video>
                     </li>
                 </ul>
-                <ul class="con-list" style="display:none;">
+                <ul class="con-list photo-list" style="display:none;">
                     <li class="con-item photo-pic" v-for="(item,index) in photoList" :key="index">
                         <img v-lazy="item.imgSrc" alt="">
                     </li>
@@ -119,7 +119,44 @@ export default {
                 },
                 {
                     imgSrc:'/static/images/st93.png'
-                }
+                },
+                {
+                    imgSrc:'https://www.topber.com/public/images/box.png'
+                },
+                {
+                    imgSrc:'https://pic.zhuayoukong.com/d/file/game/2017-04-11/3d0e22247f98129a1040b2f452e2a5f7.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c543810fd3.jpg'
+                },
+                {
+                    imgSrc:'https://www.topber.com/templates/default/Public/style/img/A2.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c52fd9e71e.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c53afdf705.png'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/20/5c1b3c1e9f41d.png'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c5518aad7b.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/20/5c1b52679c531.png'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c4e38c5df9.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c4c3b2f8ab.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/19/5c19e5a8a5aa0.jpg'
+                },
+
             ]
         }
     },
@@ -237,4 +274,8 @@ export default {
         padding: 0 50px;
         line-height: 50px;
     }
+    /* .photo-list{
+        column-gap:10px;
+        column-count: 2;
+    } */
 </style>
--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,149 +1,125 @@
 <template>
   <div class="hello">
-    <HeaderCom :tit="title" :backShow="backShow"/>
-    <h1>{{ msg }}</h1>
-    <h2>{{love}}</h2>
-    <h3>{{msg}}</h3>
-    <h4>{{msg}}</h4>
-    <h5>{{hate}}</h5>
-    <h6>{{love}}</h6>
-    <h2>H2标题栏</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-    <div class="box">
-      <div class="game-list" v-for="(item,index) in gameList" :key="index">
-        <a href="https://www.baidu.com?">{{index}}:{{item.name}}&ensp;&ensp;&ensp;&ensp;{{item.price}}</a>
-      </div>
+    <HeaderCom :tit="title" :backShow="backShow" />
+    <div class="con-box">
+      <ul class="con-list photo-list">
+        <li class="con-item photo-pic" v-for="(item,index) in photoList" :key="index">
+          <img v-lazy="item.imgSrc" alt="">
+        </li>
+      </ul>
     </div>
     <NavCom></NavCom>
   </div>
 </template>
 
 <script>
-import NavCom from "@/components/NavCom";
-import HeaderCom from "@/components/HeaderCom";
-import '../../static/js/snowstorm.js';
-export default {
-  name: 'Photo',
-  data () {
-    return {
-      title:"照片墙",
-      backShow:false,
-      msg: 'Welcome to Your Vue.js App',
-      love: '我喜欢vue',
-      hate: '我讨厌react',
-      gameList: [
-        {name: '上衣',price: '$189'},
-        {name: '裤子',price: '$289'},
-        {name: '帽子',price: '$39'},
-        {name: '帽子',price: '$189'},
-        {name: '帽子',price: '$189'},
-        {name: '帽子',price: '$189'},
-        {name: '帽子',price: '$189'},
-        {name: '帽子',price: '$189'},
-        {name: '帽子',price: '$189'},
-        {name: '帽子',price: '$189'},
-      ]
+  import NavCom from "@/components/NavCom";
+  import HeaderCom from "@/components/HeaderCom";
+  export default {
+    name: 'Photo',
+    data() {
+      return {
+        title: "照片墙",
+        backShow: false,
+        photoList:[
+                {
+                    imgSrc:'/static/images/bg.jpg'
+                },
+                {
+                    imgSrc:'/static/images/ykj.jpg'
+                },
+                {
+                    imgSrc:'/static/images/dwh.jpg'
+                },
+                {
+                    imgSrc:'/static/images/wx.jpg'
+                },
+                {
+                    imgSrc:'/static/images/wx-dwh.jpg'
+                },
+                {
+                    imgSrc:'/static/images/st93.png'
+                },
+                {
+                    imgSrc:'https://www.topber.com/public/images/box.png'
+                },
+                {
+                    imgSrc:'https://pic.zhuayoukong.com/d/file/game/2017-04-11/3d0e22247f98129a1040b2f452e2a5f7.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c543810fd3.jpg'
+                },
+                {
+                    imgSrc:'https://www.topber.com/templates/default/Public/style/img/A2.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c52fd9e71e.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c53afdf705.png'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/20/5c1b3c1e9f41d.png'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c5518aad7b.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/20/5c1b52679c531.png'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c4e38c5df9.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/21/5c1c4c3b2f8ab.jpg'
+                },
+                {
+                    imgSrc:'https://img.topber.com/upload/apk/2018/12/19/5c19e5a8a5aa0.jpg'
+                },
+
+            ]
+      }
+    },
+    components: {
+      NavCom,
+      HeaderCom
     }
-  },
-  components :{
-    NavCom,
-    HeaderCom
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  text-decoration: none;
-  color: #42b983;
-}
+  .con-box{
+    padding: 5px;
+    overflow: hidden;
+    box-sizing: border-box;
+  }
+  .photo-list{
+        column-gap:10px;
+        -moz-column-count:2; /* Firefox */
+        -webkit-column-count:2; /* Safari 和 Chrome */
+        column-count:2;
+        column-rule-style: dashed;
+        column-rule-color: rgba(255,255,255,0.8);
+    }
+    .con-list{
+      box-sizing: border-box;
+        overflow: hidden;
+        width: 100%;
+    }
+    .con-list .con-item{
+      box-sizing: border-box;
+        background: #fff;
+        padding: 0px 5px;
+        margin-bottom: 5px;
+        border-radius: 5px;
+        border-top: 5px solid #fff;
+        border-bottom: 5px solid #fff;
+        break-inside: avoid;
+    }
+    .con-list .con-item.photo-pic img{
+        display: block;
+        width: 100%;
+    }
 </style>
